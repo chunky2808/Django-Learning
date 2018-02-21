0d63944831acd00666e57ffddf2d3e67d27e1ea3
chunky2808 <parasjain2896@gmail.com>
2018-02-21 20:16:41 +0530

    third basic template done site working

--- enlight/views.py
+++ enlight/views.py
@@ -5,10 +5,4 @@ from .models import Forum
 # Create your views here.
 def home(request):
 	forums = Forum.objects.all()
-	forum_name = list()
-
-	for forum in forums:
-		forum_name.append(forum.name)
-
-	response_html = '<br>'.join(forum_name)	
-	return HttpResponse(response_html)
+	return render(request,'home.html',{'forums' : forums})
