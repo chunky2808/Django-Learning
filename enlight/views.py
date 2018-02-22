ef83af8988381e24226372fffccf6c2009c6b36b
chunky2808 <parasjain2896@gmail.com>
2018-02-22 20:18:00 +0530

    seventh-Model form added

--- enlight/views.py
+++ enlight/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render,get_object_or_404,redirect
 from django.contrib.auth.models import User
 from django.http import HttpResponse,Http404
+from .forms import NewTopicForm
 from .models import Forum,Topic,Post
 
-# Create your views here.
+
 def home(request):
 	forums = Forum.objects.all()
 	return render(request,'home.html',{'forums' : forums})
@@ -12,26 +13,22 @@ def forum_topics(request,pk):
 	forums = get_object_or_404(Forum, pk=pk)
 	return render(request,'topics.html',{'forums': forums})
 
-def new_topic(request,pk):
+def new_topic(request, pk):
 	forums = get_object_or_404(Forum, pk=pk)
+	user = User.objects.first()  # TODO: get the currently logged in user
 	if request.method == 'POST':
-		subject = request.POST['subject']
-		message = request.POST['message']
-
-		user = User.objects.first()  # TODO: get the currently logged in user
-
-		topic = Topic.objects.create(
-			subject=subject,
-			forum=forums,
-			started_by=user
+		form = NewTopicForm(request.POST)
+		if form.is_valid():
+			topic = form.save(commit=False)
+			topic.forum = forums
+			topic.started_by = user
+			topic.save()
+			post = Post.objects.create(
+				message=form.cleaned_data.get('message'),
+				topic=topic,
+				created_by=user
 			)
-
-		post = Post.objects.create(
-			message=message,
-			topic=topic,
-			created_by=user
-			)
-
-		return redirect('forum_topics', pk=forums.pk)  # TODO: redirect to the created topic page
-
-	return render(request,'new_topics.html',{'forums':forums})
+			return redirect('forum_topics', pk=forums.pk)  # TODO: redirect to the created topic page
+	else:
+		form = NewTopicForm()
+	return render(request, 'new_topics.html', {'forums': forums, 'form': form})
